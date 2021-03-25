baf41befb15b64f80d3c7243ef6c25398283d30c
Carlos Hilner <hilner@lu.ufc.br>
2021-03-25 15:02:32 -0300

    material-ui configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import {Button} from '@material-ui/core'
 
 export default function Home() {
-  return <div>Clone Youtube + NextJS + MaterialUI</div>
+
+  return (
+  <div>
+    Clone Youtube + NextJS + MaterialUI
+    <Button color="primary">Hello button</Button>
+    <Button variant="contained" color="secondary" href="#contained-buttons">
+  Link
+</Button>
+    </div>
+    )
 }
